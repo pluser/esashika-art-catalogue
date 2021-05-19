4e444454e7aadf806f037873ab3a1d16662a5500
ESASHIKA Kaoru <kaoru@esashika.net>
2021-05-20 05:21:08 +0900

    add image field

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Opus(models.Model):
     opus_title = models.CharField(max_length=200)
     opus_description = models.TextField()
+    opus_image = models.ImageField(upload_to='opus_image', null=True)
 
     class Meta:
         verbose_name_plural = 'Opuses'
