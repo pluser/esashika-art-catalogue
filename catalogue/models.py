324ebd71906ad44615634afd55f0e0fe8c4580f1
ESASHIKA Kaoru <kaoru@esashika.net>
2021-05-20 02:37:11 +0900

    rename database

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class Opuses(models.Model):
+class Opus(models.Model):
     opus_title = models.CharField(max_length=200)
     opus_description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Opuses'
 
     def __str__(self):
         return self.opus_title
