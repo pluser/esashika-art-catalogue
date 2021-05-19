36d503bc20e7966165313ffe8de4c44721ed54ba
ESASHIKA Kaoru <kaoru@esashika.net>
2021-05-20 02:25:32 +0900

    add fancy text method

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Opuses(models.Model):
     opus_title = models.CharField(max_length=200)
     opus_description = models.TextField()
+
+    def __str__(self):
+        return self.opus_title
